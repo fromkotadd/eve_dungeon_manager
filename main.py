e485c3db172377111e3d01b29b9c568b43128fa9
Evgeniy <kotadd2@gmail.com>
2023-02-20 10:49:50 +0200

    все переписал с 3 конструкторами

--- main.py
+++ main.py
@@ -2,16 +2,18 @@ import sqlite3
 
 
 class Person:
-	def __init__(self, **kwargs):
-		self.pilot_name = kwargs.get('pilot_name', 'None')
-		self.ship_type = kwargs.get('ship_type', 'None')
-		self.first_skill = kwargs.get('first_skill', 'None')
-		self.second_skill = kwargs.get('second_skill', 'None')
-		self.implant_lvl = kwargs.get('implant_lvl', 'None')
-		self.count_entry = kwargs.get('count_entry', 'None')
-		self.id = kwargs.get('id', 'None')
-		self.fit_class = kwargs.get('fit_class', 'None')
-		self.pilot_rating = kwargs.get('pilot_rating', 'None')
+	def __init__(self):
+		# Представление карточки пилота для юзера
+		self.pilot_name = None
+		self.ship_type = None
+		self.first_skill = None
+		self.second_skill = None
+		self.implant_lvl = None
+		self.fit_class = None
+		self.pilot_rating = None
+		# данные для БД
+		self.count_entry = None
+		self.id = None
 
 	def pilot_card_builder(self):
 		return {
@@ -20,16 +22,77 @@ class Person:
 			'first_skill': self.first_skill,
 			'second_skill': self.second_skill,
 			'implant_lvl': self.implant_lvl,
-			# 'count_entry': self.count_entry,
-			# 'id': self.id,
 			'fit_class': self.fit_class,
-			'pilot_rating': self.pilot_rating
+			'pilot_rating': self.pilot_rating,
+			'count_entry': self.count_entry,
+			'id': self.id,
 		}
-
 	def __str__(self):
 		return f"pilot name: {self.pilot_name}, ship type: {self.ship_type}, first skill: {self.first_skill}," \
-			   f" second skill: {self.second_skill}, implant lvl: {self.implant_lvl}, count entry: {self.count_entry}," \
-			   f" id: {self.id}, fit class: {self.fit_class}, pilot rating: {self.pilot_rating}"
+				f" second skill: {self.second_skill}, implant lvl: {self.implant_lvl},fit class: {self.fit_class}," \
+				f" pilot rating: {self.pilot_rating}, count entry: {self.count_entry}, id: {self.id},"
+
+
+class PersonBuilder:
+	def __init__(self, person=Person()):
+		self.person = person
+
+	@property
+	def for_initialization(self):
+		return PersonBuilderForInitialization(self.person)
+
+	@property
+	def for_download_to_database(self):
+		return PersonBuilderFromDb(self.person)
+
+	def build(self):
+		return self.person
+
+
+class PersonBuilderForInitialization(PersonBuilder):
+	def __init__(self, person=Person()):
+		super().__init__(person)
+
+	def pilot_name(self, pilot_name):
+		self.person.pilot_name = pilot_name
+		return self
+
+	def ship_type(self, ship_type):
+		self.person.ship_type = ship_type
+		return self
+
+	def first_skill(self, first_skill):
+		self.person.first_skill = first_skill
+		return self
+
+	def second_skill(self, second_skill):
+		self.person.second_skill = second_skill
+		return self
+
+	def implant_lvl(self, implant_lvl):
+		self.person.implant_lvl = implant_lvl
+		return self
+
+	def fit_class(self, fit_class):
+		self.person.fit_class = fit_class
+		return self
+
+	def pilot_rating(self, pilot_rating):
+		self.person.pilot_rating = pilot_rating
+		return self
+
+
+class PersonBuilderFromDb(PersonBuilder):
+	def __init__(self, person):
+		super().__init__(person)
+
+	def count_entry(self, count_entry):
+		self.person.count_entry = count_entry
+		return self
+
+	def id(self, id):
+		self.person.id = id
+		return self
 
 
 class DownloadAdaptor:
@@ -42,7 +105,7 @@ class DownloadAdaptor:
 		for key, value in self.pilot_card_dist.items():
 			self.table_cell_name.append(key)
 			self.cell_content.append(value)
-		return self.table_cell_name, self.cell_content
+		return self.table_cell_name[:-1], self.cell_content[:-1]
 
 	def __str__(self):
 		return self.pilot_card_dist
@@ -57,7 +120,7 @@ class DbManager:
 		return sqlite_connection, cursor
 
 	@staticmethod
-	def damp_to_db(adaptor_lst, db_name='pilot.db'):
+	def damp_to_db(adaptor_lst, db_name):
 		sqlite_connection, cursor = DbManager.db_connect(db_name)
 		param_names = [f"p{i}" for i in range(len(adaptor_lst[1]))]
 		sqlite_insert_with_param = f"""INSERT INTO pilot_card
@@ -70,17 +133,17 @@ class DbManager:
 		cursor.close()
 
 	@staticmethod
-	def load_from_db(pilot_id, db_name='pilot.db'):
+	def load_from_db(pilot_id, db_name):
 		sqlite_connection, cursor = DbManager.db_connect(db_name)
 		sqlite_select_query = """SELECT * from pilot_card where id = ?"""
-		cursor.execute(sqlite_select_query, (pilot_id, ))
+		cursor.execute(sqlite_select_query, (pilot_id,))
 		print("Чтение одной строки \n")
 		record = cursor.fetchone()
 		cursor.close()
 		return record
 
 	@staticmethod
-	def table_update(table_name, pilot_id, changes, db_name='pilot.db'):
+	def table_update(table_name, pilot_id, changes, db_name):
 		sqlite_connection, cursor = DbManager.db_connect(db_name)
 		sql_update_query = f"""Update pilot_card set {table_name} = ? where id = ?"""
 		cursor.execute(sql_update_query, (changes, pilot_id))
@@ -90,12 +153,10 @@ class DbManager:
 
 
 if __name__ == "__main__":
-	# p = Person(pilot_name='bobi', ship_type='бш', second_skill='554', first_skill='333', pilot_rating='low', implant_lvl='10',
-	# 			fit_class='c')
-	# card = p.pilot_card_builder()
-	# print(p.pilot_card_builder())
-	# print(p)
-	# s = DownloadAdaptor(card).adaptor()
-	# DbManager.damp_to_db(s, 'pilot.db')
-	# DbManager.table_update(table_name='implant_lvl', changes=7, pilot_id=1)
-	DbManager.load_from_db(32)
+	pb = PersonBuilder()
+	person = pb.for_initialization.pilot_name('Riva25 Wilson').ship_type('bk').first_skill('553').second_skill('553')\
+		.implant_lvl('10').fit_class('c').pilot_rating('mid').for_download_to_database.count_entry('10').build()
+	card = person.pilot_card_builder()
+	pilot_card_to_dump = DownloadAdaptor(card).adaptor()
+	DbManager().damp_to_db(pilot_card_to_dump, 'pilot.db')
+
