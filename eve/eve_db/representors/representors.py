3a578d98a7cb3715b019564ef38c80730af7b49f
Evgeniy <kotadd2@gmail.com>
2023-03-11 13:08:12 +0200

    переходный

--- eve/eve_db/representors/representors.py
+++ eve/eve_db/representors/representors.py
@@ -1,9 +1,11 @@
 from typing import Optional
 
 from django.db.models import QuerySet
+from django.forms.models import model_to_dict
+
 
 from eve_db.selectors.pilot import pilots_for_first_dungeon
-from eve_db.models import Skill, PilotShip, Implant, Pilot
+from eve_db.models import Pilot
 
 
 def foo() -> Optional[str]:
@@ -11,9 +13,12 @@ def foo() -> Optional[str]:
 
 	for p in sample_of_pilots:
 		pilot = Pilot.objects.get(discord_id=p['discord_id'])
-		pilot_ships = pilot.pilot_ships.filter().values()
+		pilot_ships = pilot.pilot_ships.get()
 		pilot_skills = pilot.skills.filter().values()
 		pilot_implant = pilot.implants.filter().values()
-		print(Pilot.objects.filter(discord_id=p['discord_id']), pilot_ships, pilot_skills, pilot_implant)
+		# print(Pilot.objects.filter(discord_id=p['discord_id']), pilot_ships, pilot_skills, pilot_implant)
 
+		s = model_to_dict(pilot_ships, fields=None, exclude=None)
+		print(s)
+		print(type(s))
 
