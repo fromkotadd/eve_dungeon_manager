409d99f6fd88313af693f1c4d014104a40e02a3d
Evgeniy <kotadd2@gmail.com>
2023-12-17 13:53:20 +0200

    мерж слеш и кнопочного бота

--- eve/eve_db/discord_api/config.py
+++ eve/eve_db/discord_api/config.py
@@ -1,5 +1,7 @@
+import os
+
 config = {
-	'TOKEN': '',
+	'TOKEN': os.environ.get('TOKEN'),
 	'ID_CHANNEL': '1084257606740213821',
 	'OWNER_ID': 699256655837397132,
 	'GUILD_ID': 1065239528664739870,
