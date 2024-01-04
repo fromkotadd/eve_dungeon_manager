7a883e829fa7579816f8f49c413ff0c34ecb8979
Evgeniy <kotadd2@gmail.com>
2024-01-04 15:39:12 +0200

    принудительное завершение регистрации с удалением созданных каналов/сообщений

--- eve/eve_db/discord_api/services/pilot/delete.py
+++ eve/eve_db/discord_api/services/pilot/delete.py
@@ -3,16 +3,21 @@ import discord
 
 from eve_db.discord_api.button_bot import BOT
 from eve_db.representors.representors import pilot_card_delete
+from eve_db.discord_api.config import config
 
 class PilotCardDelete():
 	def __init__(self, interaction: discord.Interaction):
 		self.interaction = interaction
 		self.discord_id = self.interaction.user.id
+		self.user = self.interaction.user
+		self.role = self.interaction.guild.get_role(config['DORMANT_ROLE'])
+
 
 	async def pilot_card_delete(self):
 		result = await pilot_card_delete(
 			discord_id=self.discord_id
 		)
+		await self.user.remove_roles(self.role)
 		await self.interaction.response.send_message(result, ephemeral=True)
 		await asyncio.sleep(10)
 		await self.interaction.delete_original_response()
